0ed103c3b854d6c275f52b6f7a3ab6cfb026b5d3
brownjenjen <brownjenjen01@outlook.com>
2016-05-01 20:48:00 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -205,7 +205,8 @@ $(document).on('touchstart click', '*', function(event){
 			//var x= event.clientX;
 			//var y= event.clientY;
 			var track = title+'|'+term+'|'+time+'|'+event.target.nodeName+'|'+href+'|'+window.location.href+'|'+tag+'|'+document.referrer+'|'+ua+'|';
-			_gaq.push(['_trackEvent',window.location.hostname,tag+"|"+parseInt(cachedX)+"|"+parseInt(cachedY)+"|"+width+"("+screen.width+")|"+height+"("+screen.height+")|"+type ,track+"||"+user]); 
+			//_gaq.push(['_trackEvent',window.location.hostname,tag+"|"+parseInt(cachedX)+"|"+parseInt(cachedY)+"|"+width+"("+screen.width+")|"+height+"("+screen.height+")|"+type ,track+"||"+user]); 
+			_gaq.push(['_trackEvent',window.location.hostname,window.location.href+"|"+parseInt(cachedX)+"|"+parseInt(cachedY)+"|"+width+"|"+height+"|"+screen.width+"|"+screen.height+"|"+type ,track+"||"+user]); 
 	//	 console.log('_trackEvent',window.location.hostname+'|'+tag, track, user);
 			 //setTimeout(function() { // now wait 300 milliseconds...
 			//	window.open(href,(!target?"_self":target)); // ...and open the link as usual
