506857e19090b2301557828b591374f0948d187b
brownjenjen <brownjenjen01@outlook.com>
2016-05-01 16:11:18 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -23,7 +23,7 @@
   return false;
 }
 var sites='';
-var ua='':
+var ua='';
 var customizeForDevice = function(){
     ua = navigator.userAgent;
     var checker = {
@@ -75,7 +75,7 @@ arr =   data;
 console.log(arr);
 var entryHTML ='';
 var link = '';
-$("#iphone_title").html("<div class='sidebar section' ><div class='section_title small single_block'><h2 class='title' style='padding: 0;margin: 0;'><a href='http://"+arr['tumblelog']['name']+".tumblr.com' >"+arr['tumblelog']['title'].replace(/^(.{25}[^\s]*).*/, "$1")+"...</a></h2></div></div>");
+$("#iphone_title").html("<div class='sidebar section' ><div class='section_title small single_block'><h2 class='title' style='padding: 0;margin: 0;'><a href='http://"+arr['tumblelog']['name']+".tumblr.com' >"+arr['tumblelog']['title'].replace(/^(.{25}[^\s]*).*/, "$1")+"</a></h2></div></div>");
 //for (var i=0;i< arr['posts'].length;i++)
 for (var i=0;i< arr['posts'].length;i++)		 
 {
