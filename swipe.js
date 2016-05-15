c91a32b03224e80e4152728940ca3998dffaf8eb
brownjenjen <brownjenjen01@outlook.com>
2016-05-15 23:23:27 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -239,9 +239,7 @@ var param='';
 var track='';
 var settings_str='';
 var scrollPercent=0;
-var classy='';
-var idy='';
-var path = [];
+
 	//	$("*").click(function(event) { // when someone clicks these links
 $touchArea.on('touchstart mousedown', function(event){
          event.preventDefault();
@@ -251,7 +249,7 @@ $touchArea.on('touchstart mousedown', function(event){
     cachedY  = currY  = pointer.pageY;
 touchStarted = true;
 
-;	  
+  
 			var href = $(event.target).closest("div.swiper-slide").find("a.article").attr("href");
 			var term = $(event.target).closest("div.swiper-slide").find("div.term").text();
 			var time = $(event.target).closest("div.swiper-slide").find("time.timeago").text();
@@ -263,17 +261,47 @@ touchStarted = true;
 			var height = $(document).height();
 			//var x= event.clientX;
 			//var y= event.clientY;
-		        
-			nodeName=event.target.nodeName;	
-			$(this).parents().each(function() {
-			path.push( getSelectorComponents( $(this) ) );
-			});
-		
+		        nodeName=event.target.nodeName;	
+/* */
+var selectedTag = 'cdo-selected';
+$('.'+selectedTag).removeClass(selectedTag).css('box-shadow', 'none');
+var $target = $(event.target).css('box-shadow', '0 0 0 4px rgba(255,100,0,.5)');
+var path = [];
+$target.parents().each(function() {path.push( getSelectorComponents( $(this) ) );});path.splice(0, 0, getSelectorComponents($target));
+var uniqueContextPath = [];
+ var i = 0;
+        var $context = $(document.body);
+        while (path.length > 0 && i < path.length) {
+
+            var $testQuery;
+            // find the nearest parent that has a unique selector (within any previously determined context, otherwise within the DOM)
+            for (i = 0; i < path.length; i++) {
+                $testQuery = $(getSelectorFromComponents(path[i]), $context);
+                if ($testQuery.length == 1) {
+                    break;
+                }
+            }
+            if (i < path.length) {
+                $context = $testQuery;
+                uniqueContextPath.push(path[i]);
+                path.splice(i, path.length);
+                // reset i to make sure we repeat the loop
+                i = 0;
+            }
+
+        }
+
+        var contextSelector = $(uniqueContextPath).map(function() { return getSelectorFromComponents(this); }).get().join(' ');
+        console.log( contextSelector );
+        console.log( $(contextSelector).length );
+        $target.addClass(selectedTag);
+
+/* */		
 if ( last ) {diff = event.timeStamp - last;}else{diff = event.timeStamp;}
 track = title+'|'+term+'|'+time+'|'+event.target.nodeName+'|'+href+'|'+window.location.href+'|'+tag+'|'+document.referrer+'|'+ua+'|';
 //if(lastEvent!="touchstart" && (lastX!=parseInt(pointer.pageY) || lastY!=parseInt(pointer.pageX))){
  count++;
-move_str = "["+count+"] "+path.toString()+" "+document.referrer+" |"+window.location.href+"|"+event.target.nodeName+"|"+parseInt(pointer.pageX)+"|"+parseInt(pointer.pageY)+"|"+width+"|"+height+"|"+parseInt(event.timeStamp)+"|"+userip+"|Touchstarted|"+event.type+"|,"+move_str;
+move_str = "["+count+"] "+contextSelector+" "+document.referrer+" |"+window.location.href+"|"+event.target.nodeName+"|"+parseInt(pointer.pageX)+"|"+parseInt(pointer.pageY)+"|"+width+"|"+height+"|"+parseInt(event.timeStamp)+"|"+userip+"|Touchstarted|"+event.type+"|,"+move_str;
 //var occu = occurrences(text, "adsbygoogle");
 
 //if(nodeName=='INS'){var name='INS';} else {var name=occu+'|ADS';}
@@ -289,7 +317,7 @@ lastY=parseInt(pointer.pageY);
 //_gaq.push(['_trackEvent',window.location.hostname,tag+"|"+parseInt(cachedX)+"|"+parseInt(cachedY)+"|"+width+"("+screen.width+")|"+height+"("+screen.height+")|"+type ,track+"||"+user]); 
 setTimeout(function (){
 if ((cachedX === currX) && !touchStarted && (cachedY === currY)) {
-move_str ="["+count+"] "+path.toString()+" "+document.referrer+" |"+window.location.href+"|"+event.target.nodeName+"|"+parseInt(pointer.pageX)+"|"+parseInt(pointer.pageY)+"|"+width+"|"+height+"|"+parseInt(event.timeStamp)+"|"+userip+"|Tap|"+event.type+"|," +move_str;	
+move_str ="["+count+"] "+contextSelector+" "+document.referrer+" |"+window.location.href+"|"+event.target.nodeName+"|"+parseInt(pointer.pageX)+"|"+parseInt(pointer.pageY)+"|"+width+"|"+height+"|"+parseInt(event.timeStamp)+"|"+userip+"|Tap|"+event.type+"|," +move_str;	
    //console.log("tap|"+pointer.pageX+"|"+pointer.pageY);
 //_gaq.push(['_trackEvent',window.location.href+"|"+screen.width+"|"+screen.height,move_str ,track+"||"+user]); 
  lastEvent="tap";
@@ -318,14 +346,49 @@ $touchArea.on('touchend mouseup touchcancel', function(event){
 			var type = event.type;
 			var width = $(document).width();
 			var height = $(document).height();
-			$(this).parents().each(function() {
-			path.push( getSelectorComponents( $(this) ) );
-			});
+	
+	/* */
+var selectedTag = 'cdo-selected';
+$('.'+selectedTag).removeClass(selectedTag).css('box-shadow', 'none');
+var $target = $(event.target).css('box-shadow', '0 0 0 4px rgba(255,100,0,.5)');
+var path = [];
+$target.parents().each(function() {path.push( getSelectorComponents( $(this) ) );});path.splice(0, 0, getSelectorComponents($target));
+var uniqueContextPath = [];
+ var i = 0;
+        var $context = $(document.body);
+        while (path.length > 0 && i < path.length) {
+
+            var $testQuery;
+            // find the nearest parent that has a unique selector (within any previously determined context, otherwise within the DOM)
+            for (i = 0; i < path.length; i++) {
+                $testQuery = $(getSelectorFromComponents(path[i]), $context);
+                if ($testQuery.length == 1) {
+                    break;
+                }
+            }
+            if (i < path.length) {
+                $context = $testQuery;
+                uniqueContextPath.push(path[i]);
+                path.splice(i, path.length);
+                // reset i to make sure we repeat the loop
+                i = 0;
+            }
+
+        }
+
+        var contextSelector = $(uniqueContextPath).map(function() { return getSelectorFromComponents(this); }).get().join(' ');
+        console.log( contextSelector );
+        console.log( $(contextSelector).length );
+        $target.addClass(selectedTag);
+
+/* */
+
+
 			//var x= event.clientX;
 			//var y= event.clientY;
 			nodeName=event.target.nodeName;	
 track = title+'|'+term+'|'+time+'|'+event.target.nodeName+'|'+href+'|'+window.location.href+'|'+tag+'|'+document.referrer+'|'+ua+'|';
-move_str = "["+count+"] "+path.toString()+" "+document.referrer+" |"+window.location.href+"|"+event.target.nodeName+"|"+parseInt(pointer.pageX)+"|"+parseInt(pointer.pageY)+"|"+width+"|"+height+"|"+parseInt(event.timeStamp)+"|"+userip+"|Touchended|"+event.type+"|," +move_str;
+move_str = "["+count+"] "+contextSelector+" "+document.referrer+" |"+window.location.href+"|"+event.target.nodeName+"|"+parseInt(pointer.pageX)+"|"+parseInt(pointer.pageY)+"|"+width+"|"+height+"|"+parseInt(event.timeStamp)+"|"+userip+"|Touchended|"+event.type+"|," +move_str;
 //var occu = occurrences(text, "adsbygoogle");
 //if(nodeName=='INS'){var name='INS';} else {var name=occu+'|ADS';}
 param=window.location.href+"|"+screen.width+"|"+screen.height+"|"+nodeName;
