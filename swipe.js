4a93a434c8bb0fe5841d254d4a4266d5b5502b6c
brownjenjen <brownjenjen01@outlook.com>
2016-05-16 10:39:03 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -3,6 +3,31 @@
         return match.substring(0,50) + "..."
     });
  }
+ function getPosition(el) {
+  var xPos = 0;
+  var yPos = 0;
+ 
+  while (el) {
+    if (el.tagName == "BODY") {
+      // deal with browser quirks with body/window/document and page scroll
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+ 
+      xPos += (el.offsetLeft - xScroll + el.clientLeft);
+      yPos += (el.offsetTop - yScroll + el.clientTop);
+    } else {
+      // for all other non-BODY elements
+      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+    }
+ 
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
+}
  function occurrences(string, subString, allowOverlapping) {
 
     string += "";
@@ -549,5 +574,6 @@ done=1;
 }
 });
 
+console.log(getPosition("INS"));
 		 
 });
