61211b1fe48b5ca5a89fc06eb5666011b634b1de
brownjenjen <brownjenjen01@outlook.com>
2016-05-17 15:41:31 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -3,26 +3,7 @@
         return match.substring(0,50) + "..."
     });
  }
- function occurrences(string, subString, allowOverlapping) {
-
-    string += "";
-    subString += "";
-    if (subString.length <= 0) return (string.length + 1);
-
-    var n = 0,
-        pos = 0,
-        step = allowOverlapping ? 1 : subString.length;
-
-    while (true) {
-        pos = string.indexOf(subString, pos);
-        if (pos >= 0) {
-            ++n;
-            pos += step;
-        } else break;
-    }
-    return n;
-}
- function in_array(needle, haystack, argStrict) {
+function in_array(needle, haystack, argStrict) {
     var key = '',
     strict = !! argStrict;
   if (strict) {
