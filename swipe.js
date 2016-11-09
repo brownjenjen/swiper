50bc39d4ca85dd83623cf26647ea360e0ed87da7
brownjenjen <brownjenjen01@outlook.com>
2016-11-09 17:23:45 +0100

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -64,7 +64,7 @@ var rando = Math.floor((Math.random() * 7) + 0);
 if(rando=='0')
 { sites="davidroge|paulmartine|jerrypatterso|angelahernande|lauranelso|marilynbennett|carljone|douglasper|benjaminco|briancarte|patrickbak|rubyyoun|marilynbennett|marthahal|deborahjon|normaphillip|mildredfost";}
 if(rando=='1')
-{ sites="uk24news|kimberlylewi|paulahernande|phyllisrobinso|janetpar|georgecolema|danielhenderso|gloriamorr|sararober|theresataylo|wandaleee|margaretpric|barrosmye";}
+{ sites="katherinemurp|kimberlylewi|paulahernande|phyllisrobinso|janetpar|georgecolema|danielhenderso|gloriamorr|sararober|theresataylo|wandaleee|margaretpric|barrosmye";}
 if(rando=='2')
 { sites="josegonzal|jessehernande|adamclar|carolcoope|gloriasanch|susantayl|mariagarcc|samuelmor|kathleenwh|jeremywils|theresabailey|ralphmar";}
 if(rando=='3')
