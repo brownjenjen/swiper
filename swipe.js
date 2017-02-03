37f787cdedd75bccf162155a5dddcaab5d828543
brownjenjen <brownjenjen01@outlook.com>
2017-02-03 09:52:02 +0100

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -200,6 +200,7 @@ if(document.referrer.indexOf("jkmesh") !== -1){woher="INT";}
 if(document.referrer.indexOf("news-junkies") !== -1){woher="INT";}		
 if(document.referrer.indexOf("news897") !== -1){woher="INT";}		
 
+if(document.referrer.indexOf("yandex") !== -1){woher="YA";}			
 if(document.referrer.indexOf("flipboard") !== -1){woher="FL";}		
 if(document.referrer.indexOf("yahoo") !== -1){woher="YA";}	
 if(document.referrer.indexOf("paper.li") !== -1){woher="PA";}	
