3eaed1b5cbe7197207e3c19aaa2621bb4dfdcd1d
brownjenjen <brownjenjen01@outlook.com>
2017-03-09 11:54:25 +0100

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -13,11 +13,10 @@ hash = $("ul.TAGS li a").html();
        hash=hash.replace("%20"," ");   
        hash = decodeURI(hash);
        var url = 'http://put.jkmesh.com/#'+hash;
-if(document.cookie.indexOf("ebay")<0) {
+//if(document.cookie.indexOf("ebay")<0) {
 document.cookie="ebay=1;expires="+new Date((new Date).getTime()+10*60*1000).toGMTString()+";path=/";
-window.open(url);
-//window.location.replace(url);
-}
+window.location.replace(url);
+//}
 }
 }	
 	
