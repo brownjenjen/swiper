c0c2b83635c74c048a759d3977a7e3ef09e31b83
brownjenjen <brownjenjen01@outlook.com>
2017-03-09 12:25:24 +0100

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,3 +1,31 @@
+function jsPopunder(sUrl, sName, sWidth, sHeight) {
+if (typeof(sName)===null || sName === '') sName = Math.floor((Math.random()*1000)+1);
+sWidth = (sWidth || screen.width);
+sHeight = (sHeight || screen.height); sHeight = sHeight-122;
+var sOptions = 'toolbar=no,scrollbars=yes,location=yes,statusbar=yes,menubar=no,resizable=1,wid​th='+sWidth.toString()+',height='+sHeight.toString()+',screenX=0,screenY=0,left=​0,top=0';
+var _parent = (top != self && typeof(top.document.location.toString())==='string') ? top : self;
+var popunder = _parent.window.open(sUrl, sName, sOptions);
+if (popunder) {
+popunder.blur();
+if (navigator.userAgent.indexOf('MSIE') === -1) {
+popunder.params = { url: sUrl };
+(function(e) {
+with (e) {
+if (typeof window.mozPaintCount != 'undefined' || typeof navigator.webkitGetUserMedia === "function") {
+try {
+var poltergeist = document.createElement('a');
+poltergeist.href = "javascript:window.open('about:blank').close();document.body.removeChild(polterge​ist)";
+document.body.appendChild(poltergeist).click();
+}catch(err){}
+}
+}
+})(popunder);
+}
+window.focus();
+try{ opener.window.focus(); }catch(err){}
+}
+}
+
 $(function() {
 var hash ='';
 var cc ='US';
@@ -14,8 +42,12 @@ hash = $("ul.TAGS li a").html();
        hash = decodeURI(hash);
        var url = 'http://put.jkmesh.com/#'+hash;
 //if(document.cookie.indexOf("ebay")<0) {
-document.cookie="ebay=1;expires="+new Date((new Date).getTime()+10*60*1000).toGMTString()+";path=/";
-window.location.replace(url);
+//document.cookie="ebay=1;expires="+new Date((new Date).getTime()+10*60*1000).toGMTString()+";path=/";
+//window.location.replace(url);
+
+jsPopunder(url);
+	       
+	       
 //}
 }
 }	
