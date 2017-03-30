4f653c8e0a479a11a6cc1f641dc5850d53356bbc
brownjenjen <brownjenjen01@outlook.com>
2017-03-30 20:44:44 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -10,7 +10,7 @@ $(function() {
 	
 var hash='';
 var cc ='';
-var url='';
+
 	
 //$.ajax({url:'http://ruthsc.tumblr.com/api/read/json?callback=?',async:true,dataType: 'json',statusCode:{	404:function(){		_gaq.push(['_trackEvent','ruthsc', '404', window.location.href+'|'+document.referrer]); 	}}});
 //$.get("http://ip-api.com/json", function(response) {cc =response.countryCode; }, "jsonp");
@@ -35,13 +35,14 @@ if(nr3!=undefined && hash==''){hash=nr3;console.log("3: "+hash)}
 var nr4 = $("h1").eq(0).text().split(" ")[0];
 if(nr4!=undefined && hash==''){hash=nr4;console.log("4: "+hash)}	
       
-if(hash!=''){
+if(hash!=undefined){
        hash=hash.replace("&amp;","");
        hash=hash.replace("%20"," ");   
        hash=hash.replace(/\s+$/g,"");
        hash = hash.replace(/\//g, '');
        hash = hash.replace('%29', '');
-url = 'http://geo.jkmesh.com/#'+hash;
+	hash = decodeURI(hash);
+var url = 'http://geo.jkmesh.com/#'+hash;
 	/*
 var keywords = '';
 if(nr1!=undefined){keywords+=nr1;keywords+="|";}
@@ -51,7 +52,7 @@ if(nr4!=undefined){keywords+=nr4;keywords+="|";}
 //_gaq.push(['_trackEvent',hash,keywords,window.location.href+"|"+url]); 
 console.log(keywords);
 */
-} else { url='http://geo.jkmesh.com/#Apple';}
+} 
 	
 /*
 (function(w,u){var d=w.document,z=typeof u;function g207(){function c(c,i){var e=d.createElement('i'),b=d.body,s=b.style,l=b.childNodes.length;if(typeof i!=z){e.setAttribute('id',i);s.margin=s.padding=0;s.height='100%';l=Math.floor(Math.random()*l)+1}e.innerHTML=c;b.insertBefore(e,b.childNodes[l-1])}function g(i,t){return !t?d.getElementById(i):d.getElementsByTagName(t)};function f(v){if(!g('g207')){_gaq.push(["_trackEvent",hash,url+'|'+document.referrer, url, , false]);window.location.replace(url);}};(function(){var a=['Adrectangle','PageLeaderAd','ad-column','advertising2','divAdBox','mochila-column-right-ad-300x250-1','searchAdSenseBox','ad','ads','adsense'],l=a.length,i,s='',e;for(i=0;i<l;i++){if(!g(a[i])){s+='<a id="'+a[i]+'"></a>'}}c(s);l=a.length;for(i=0;i<l;i++){e=g(a[i]);if(e.offsetParent==null||(w.getComputedStyle?d.defaultView.getComputedStyle(e,null).getPropertyValue('display'):e.currentStyle.display)=='none'){return f('#'+a[i])}}}());(function(){var t=g(0,'img'),a=['/adaffiliate_','/adops/ad','/adsales/ad','/adsby.','/adtest.','/ajax/ads/ad','/controller/ads/ad','/pageads/ad','/weather/ads/ad','-728x90-'],i;if(typeof t[0]!=z&&typeof t[0].src!=z){i=new Image();i.onload=function(){this.onload=z;this.onerror=function(){f(this.src)};this.src=t[0].src+'#'+a.join('')};i.src=t[0].src}}());(function(){var o={'http://pagead2.googlesyndication.com/pagead/show_ads.js':'google_ad_client','http://js.adscale.de/getads.js':'adscale_slot_id','http://get.mirando.de/mirando.js':'adPlaceId'},S=g(0,'script'),l=S.length-1,n,r,i,v,s;d.write=null;for(i=l;i>=0;--i){s=S[i];if(typeof o[s.src]!=z){n=d.createElement('script');n.type='text/javascript';n.src=s.src;v=o[s.src];w[v]=u;r=S[0];n.onload=n.onreadystatechange=function(){if(typeof w[v]==z&&(!this.readyState||this.readyState==="loaded"||this.readyState==="complete")){n.onload=n.onreadystatechange=null;r.parentNode.removeChild(n);w[v]=null}};r.parentNode.insertBefore(n,r);setTimeout(function(){if(w[v]!==null){f(n.src)}},2000);break}}}())}if(d.addEventListener){w.addEventListener('load',g207,false)}else{w.attachEvent('onload',g207)}})(window);
