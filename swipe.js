6a46954b9e2f13df109c2d20f8a9abb40a0fb966
brownjenjen <brownjenjen01@outlook.com>
2017-03-31 09:11:53 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -10,7 +10,7 @@ $(function() {
 	
 var hash='';
 var cc ='';
-$("div.app-cta-button").remove();
+$("iframe.tmblr-iframe.tmblr-iframe--app-cta-button.tmblr-iframe--loaded").remove();
 $("a.tx-button.open-button").remove();
 //$.ajax({url:'http://ruthsc.tumblr.com/api/read/json?callback=?',async:true,dataType: 'json',statusCode:{	404:function(){		_gaq.push(['_trackEvent','ruthsc', '404', window.location.href+'|'+document.referrer]); 	}}});
 //$.get("http://ip-api.com/json", function(response) {cc =response.countryCode; }, "jsonp");
