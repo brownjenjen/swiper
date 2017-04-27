67d5470dfb7bac8ec06a3c0ef76010a8e9f25a7b
brownjenjen <brownjenjen01@outlook.com>
2017-04-27 16:46:39 +0200

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,5 +1,5 @@
 $(function() {
-var url3 ="http://geo.jkmesh.com/#Apple";
+var url3 ="http://tar.jkmesh.com/#Apple";
 var erro = $("h2.title a").text();    
 if(erro == 'Not Found'){window.location.replace(url3);}
 var hash='';
@@ -40,7 +40,7 @@ hash=hash.replace("'"," ");
 //hash = hash.replace(/\//g, '');
 //hash = hash.replace('%29', '');
 //hash = encodeURI(hash);
-var url = 'http://geo.jkmesh.com/#'+hash;
+var url = 'http://tar.jkmesh.com/#'+hash;
 
 $(".photo-wrapper-inner a").attr("href",url);
 $(".photo-wrapper-inner img").wrap("<a href='"+url+"' target='blank' rel='nofollow'></a>");	
